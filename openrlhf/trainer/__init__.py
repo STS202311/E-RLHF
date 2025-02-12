cce7e8ee86e553af3730f0a84495913d94a4f5ed
仔舟 <tanyingshui.tys@alibaba-inc.com>
2025-02-12 14:15:04 +0800

    code

--- openrlhf/trainer/__init__.py
+++ openrlhf/trainer/__init__.py
@@ -2,15 +2,19 @@ from .dpo_trainer import DPOTrainer
 from .kd_trainer import KDTrainer
 from .kto_trainer import KTOTrainer
 from .ppo_trainer import PPOTrainer
+from .appo_trainer import APPOTrainer
+from .adpo_trainer import ADPOTrainer
 from .prm_trainer import ProcessRewardModelTrainer
 from .rm_trainer import RewardModelTrainer
 from .sft_trainer import SFTTrainer
 
 __all__ = [
     "DPOTrainer",
+    "ADPOTrainer",
     "KDTrainer",
     "KTOTrainer",
     "PPOTrainer",
+    "APPOTrainer",
     "ProcessRewardModelTrainer",
     "RewardModelTrainer",
     "SFTTrainer",
